ca89868a1f15f2abe2dd44f6fd114f3de485b43b
lida <li.da@schillerchina.com>
2020-11-22 18:33:16 +0800

    2020/11/22-call方法实现

--- JavaScript/call的实现原理/1. 基础实现.js
+++ JavaScript/call的实现原理/1. 基础实现.js
@@ -50,3 +50,35 @@ let cake = {
 }
 CakeFactory.Mycall(cake, 100, 50)
 console.log(cake);
+
+
+/**
+ * 进阶实现
+ * 考虑返回值
+ */
+Function.prototype.Mycall = function () {
+    // console.log(context, '\n', this)
+    const args = [...arguments]
+    const context = args.shift()
+    let result
+
+    context[this] = this
+    result = context[this](...args)
+    delete context[this]
+    
+    return result
+}
+function CakeFactory(width, height) {
+    this.width = width
+    this.height = height
+
+    return {
+        age: 110
+    }
+}
+let cake = {
+    name: 'cake'
+}
+const result = CakeFactory.Mycall(cake, 100, 50)
+console.log(result);
+console.log(cake)
